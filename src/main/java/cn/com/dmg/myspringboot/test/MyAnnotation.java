0ee3faf8070b9218ec7afd24f1d0b2fec9a73377
zhum <zhumeng@skyvis.com.cn>
2022-10-08 15:10:13 +0800

    提交 2022年10月8日 15:09:04

---
+++ src/main/java/cn/com/dmg/myspringboot/test/MyAnnotation.java
@@ -0,0 +1,11 @@
+package cn.com.dmg.myspringboot.test;
+/**
+ * @ClassName: MyAnnotation
+ * @Description: TODO
+ * @author zhum
+ * @date 2021/4/23 15:38
+ */
+
+public @interface MyAnnotation {
+
+}
